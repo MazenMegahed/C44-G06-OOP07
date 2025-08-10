69f10b370169924bb7cc1e18698a964db7f2b775
Mazen Megahed <mazen.megahed1@outlook.com>
2025-08-10 12:55:56 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            Console.WriteLine(Maths.Add(5, 50) );
+            Console.WriteLine(Maths.Subtract(5, 50)) ;
+            Console.WriteLine(Maths.Multiply(5, 50));
+            Console.WriteLine(Maths.Divide(5, 50));
+            double[] numm = { 5, 50, 5 };
+
+            Console.WriteLine(Maths.Avg(numm));
+            Console.WriteLine(Maths.Power(2, 3));
+            Maths.SHOWOPERATIONSCOUNT();
+            #endregion
         }
     }
 }
